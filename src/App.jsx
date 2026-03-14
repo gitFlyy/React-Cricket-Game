7c8c199f7ef537d6bcec226387863c0aaaa082a2
gitFlyy <your-email@example.com>
2026-03-14 12:00:47 +0500

    set up react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-black">
+      <div className="relative">
+        <p className="text-9xl font-bold text-purple-700">Hello</p>
+        <p className="absolute left-0 top-full mt-2 text-white">
+        Anonymoose is nigesh
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
